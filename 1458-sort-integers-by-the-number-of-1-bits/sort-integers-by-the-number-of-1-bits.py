39d29e6706829bce82e8f6ec57ed4385e72d8b1a
Vikas  sahu <vikasmangrora17@gmail.com>
2026-02-25 12:07:39 +0530

    Time: 8 ms (65.00%) | Memory: 12.8 MB (6.11%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.py
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.py
@@ -4,22 +4,5 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[int]
         """
-        def CountBits(num):
-            count=0
-            while num:
-                if num&1:
-                    count+=1
-                num>>=1
-            return count
-        temp={}
-        for num in arr:
-            val=CountBits(num) 
-            if val in temp:
-                temp[val].append(num)
-            else:
-                temp[val]=[num]
-        result=[]
-        for key in sorted(temp.keys()):
-            result.extend(sorted(temp[key]))
-        return result
-        
+     
+        return sorted(arr, key=lambda x: (bin(x).count('1'), x))
