fe6d15f9c1d6bd6a41be5bedb9078ffe63b46894
Vikas  sahu <vikasmangrora17@gmail.com>
2026-03-02 12:17:11 +0530

    Time: 8 ms (60.04%) | Memory: 19.8 MB (32.94%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -6,15 +6,15 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         res=[]
-        def back(i,p, t):
-            if t==target:
+        def back(i,p,t):
+            if t== target :
                 res.append(p.copy())
                 return 
-            if t>target:
-                return     
-            for j in range(i,len(candidates)):
+            if t > target :
+                return 
+            for j in range( i , len(candidates)):
                 p.append(candidates[j])
-                back(j,p,t+candidates[j])
+                back(j,p ,t+candidates[j])
                 p.pop()
         back(0,[],0)
-        return res        
+        return res                 
