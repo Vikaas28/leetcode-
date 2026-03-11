61add66dc3d491af25a0bffe506d69870cda9be0
Vikas  sahu <vikasmangrora17@gmail.com>
2026-03-11 23:05:18 +0530

    Time: 10 ms (26.87%) | Memory: 24.9 MB (18.32%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -6,4 +6,12 @@ class Solution:
         for n in nums:
             freq[n] = freq.get(n, 0) + 1
 
-        return sorted(freq, key=freq.get,reverse=True)[:k]
+        buck =[[] for _ in range(len(nums)+1)]
+        for num , i in freq.items():
+            buck[i].append(num)
+        res=[]    
+        for  j in range(len(buck)-1,0,-1):
+            for num in buck[j]:
+                res.append(num)
+                if len(res )==k:
+                    return res    
