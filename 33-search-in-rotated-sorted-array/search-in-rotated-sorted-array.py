7259b06827b5ee2b08d7950c2c7ccd6c306c77cc
Vikas  sahu <vikasmangrora17@gmail.com>
2026-03-31 23:49:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (59.26%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -5,24 +5,18 @@ class Solution:
 
         l=0
         h=len(arr)-1
-        while (l<=h):
+        while(l<=h):
             mid=(l+h)//2
-            if arr[mid]==t:
+            if arr[mid]  == t:
                 return mid
-            if arr[l]<=arr[mid]:
-                if arr[l]<=t and t<arr[mid]:
+            if arr[mid] >= arr[l]:
+                if arr[l]<=t and arr[mid]>t:
                     h=mid-1
                 else:
                     l=mid+1
             else:
-
-
-                if arr[mid]< t and arr[h]>=t:
+                if arr[mid]< t and arr[h] >= t :
                     l=mid+1
                 else:
                     h=mid-1
-        return -1
-
-
-
-                  
+        return - 1                           
