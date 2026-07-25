a7c06e90aaa5d345754e85f9cf2fdd3db9d8fcd1
Vikas  sahu <vikasmangrora17@gmail.com>
2026-07-25 16:13:50 +0530

    Time: 34 ms (57.20%) | Memory: 23 MB (45.17%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        # summ =0
+        # count=0
+        # l=0
+
+        # for r in range(len(nums)):
+        #     summ+=nums[r]
+        #     while summ>goal:
+        #         count+=1
+        # return count         
+        #pattern 3 count --> prefix + hasp map
+        prefix=0
+        mp={0:1}
+        count=0
+        for x in nums:
+            prefix+=x
+            if prefix-goal in mp:
+                count +=mp.get(prefix-goal,0)
+            mp[prefix]=mp.get(prefix,0)+1
+        return count         
