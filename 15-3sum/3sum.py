04f5c10784d59a2d661129be6189af9168522089
Vikas  sahu <vikasmangrora17@gmail.com>
2026-07-29 22:27:01 +0530

    Time: 495 ms (88.75%) | Memory: 22.2 MB (81.35%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -6,33 +6,34 @@ class Solution(object):
         """
         nums.sort()
         triplets = []
-        
-        for i in range(len(nums) - 2):
-            # Skip duplicate values for the first element
-            if i > 0 and nums[i] == nums[i - 1]:
+        #lst=[]
+        l=0 
+        #h=len(nums)-1
+
+
+        for i in range(len(nums)-2):
+            if nums[i]>0:
+                break
+            if i>0 and nums[i]==nums[i-1]:
                 continue
+            l=i+1
+            h=len(nums)-1
+            while l<h:
+                total=nums[i]+nums[l]+nums[h]
+                if total==0:
+                    triplets.append([nums[i],nums[l],nums[h]])
+                    while l<h and nums[l]==nums[l+1]:
+                        l+=1
+                    while l<h and nums[h]==nums[h-1]:
+                        h-=1
+                    l+=1
+                    h-=1
+                elif total<0:
+                    l+=1
+                else :
+                    h-=1                    
+
+
             
-            left, right = i + 1, len(nums) - 1
-            
-            while left < right:
-                current_sum = nums[i] + nums[left] + nums[right]
-                
-                if current_sum < 0:
-                    left += 1
-                elif current_sum > 0:
-                    right -= 1
-                else:
-                    # Found a triplet
-                    triplets.append([nums[i], nums[left], nums[right]])
-                    
-                    # Skip duplicates for the second element
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    # Skip duplicates for the third element
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    
-                    left += 1
-                    right -= 1
         
-        return triplets
+        return triplets            
