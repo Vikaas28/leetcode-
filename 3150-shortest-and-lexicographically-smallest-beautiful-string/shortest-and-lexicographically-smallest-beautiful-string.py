d075860978987129e53c75c942007bbb58c77b7c
Vikas  sahu <vikasmangrora17@gmail.com>
2026-08-26 13:42:41 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (30.63%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
@@ -1,14 +1,30 @@
 class Solution:
     def shortestBeautifulSubstring(self, s: str, k: int) -> str:
-        lenn=0
-        ans=""
+        # lenn=0
+        # ans=""
         
-        for i in range(len(s)):
-            for j in range(i ,len(s)):
-                sub=s[i:j+1]
-                lenn=i-j+1
-                if sub.count("1")==k:
-                    if  not ans or len(sub)<len(ans) or len(sub) ==len(ans) and sub < ans :
-                        ans=sub
+        # for i in range(len(s)):
+        #     for j in range(i ,len(s)):
+        #         sub=s[i:j+1]
+        #         lenn=i-j+1
+        #         if sub.count("1")==k:
+        #             if  not ans or len(sub)<len(ans) or len(sub) ==len(ans) and sub < ans :
+        #                 ans=sub
 
-        return ans 
+        # reurn ans 
+        l=0
+        count=0
+        mn=""
+        for  i in range(len(s)):
+            if s[i]=="1":
+                count+=1
+
+
+            while count==k:
+                sub=s[l:i+1]
+                if not mn or len(sub)<len(mn) or len(sub)==len(mn) and sub < mn:
+                    mn=sub
+                if s[l]=="1" :
+                    count-=1
+                l+=1
+        return mn            
