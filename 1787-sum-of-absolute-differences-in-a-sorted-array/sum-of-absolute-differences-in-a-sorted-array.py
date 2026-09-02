e040f37557dbfeb95937350b39fdd092c6049d30
Vikas  sahu <vikasmangrora17@gmail.com>
2026-09-02 17:19:30 +0530

    Time: 551 ms (95.06%) | Memory: 35 MB (15.06%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.py
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.py
@@ -15,12 +15,18 @@ class Solution:
         
         prefix_sum = [0]*len(nums)
         prefix_sum[0]=nums[0]
+        n=len(nums)
         for i in range(1,len(nums)):
             prefix_sum[i]=prefix_sum[i-1]+nums[i]
         arr = []
         for i in range(len(nums)):
-            left = ((nums[i]*(i+1) )-prefix_sum[i])
-            right = (nums[i]*(len(nums) - (i+1))) - (prefix_sum[-1] - prefix_sum[i])
+            left_sum = prefix_sum[i - 1] if i > 0 else 0
+            left = (nums[i] * i) - left_sum
+            
+            # Elements strictly to the right: (n - i - 1) elements, sum is total - prefix_sum[i]
+            right_sum = prefix_sum[-1] - prefix_sum[i]
+            right = right_sum - (nums[i] * (n - i - 1))
+
             term = abs(left) + abs(right) 
             arr.append(term)
         return arr         
